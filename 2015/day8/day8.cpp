3ed744dc7addc3b5e8c73b98907fca0ff4d62dc9
Kenneth Gorking <dasapfe@yahoo.dk>
2020-01-15 00:30:44 +0100

    day8 done

---
+++ 2015/day8/day8.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include "input.h"
+
+// Calls the function f on each part of the string divided by the 'splitter' character
+template <typename Fn>
+void for_each_part(std::string_view s, char splitter, Fn f) {
+	int offset = 0;
+	int index = s.find_first_of(splitter, 0);
+	while (index != std::string_view::npos) {
+		std::string_view part = s.substr(offset, index - offset);
+
+		f(part);
+
+		offset = index + 1;
+		index = s.find_first_of(splitter, offset);
+	}
+
+	f(s.substr(offset));
+}
+
+int main() {
+	int code_length = 0;
+	int mem_length = 0;
+
+	// \\ \" \x00
+	for_each_part(input, '\n', [&](std::string_view  line) {
+		for (int i = 0; i < line.length(); i++) {
+			switch (line[i]) {
+				case '\"':
+					code_length++;
+					break;
+
+				case '\\': {
+					switch (line[i + 1]) {
+						case '\\':
+						case '"':
+							code_length += 2;
+							mem_length += 1;
+							i++;
+							break;
+
+						case 'x':
+							code_length += 4;
+							mem_length += 1;
+							i += 3; // x88
+							break;
+					}
+
+					break;
+				}
+
+				default: { // simple character
+					code_length++;
+					mem_length++;
+					break;
+				}
+			}
+		}
+	});
+
+	std::cout << code_length - mem_length << '\n';
+
+	int encode_length = 0;
+	for_each_part(input, '\n', [&](std::string_view  line) {
+		encode_length += 2; // 2x \"
+
+		for (int i = 0; i < line.length(); i++) {
+			switch (line[i]) {
+				case '"':
+					encode_length += 2;
+					break;
+
+				case '\\':
+					encode_length += 2;
+					break;
+
+				default: { // simple character
+					encode_length++;
+					break;
+				}
+			}
+		}
+	});
+
+	std::cout << encode_length - code_length << '\n';
+}
+
