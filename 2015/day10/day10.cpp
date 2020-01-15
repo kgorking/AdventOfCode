7e3f51267f3add4b9719abb1a852715723bf2b18
Kenneth Gorking <dasapfe@yahoo.dk>
2020-01-15 16:34:17 +0100

    day 10 don

---
+++ 2015/day10/day10.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+std::string look_and_say(std::string const& input) {
+	int count = 1;
+	char last_c = input[0];
+
+	std::string result;
+	for (size_t i = 1; i < input.size(); i++) {
+		if (input[i] == last_c) {
+			count++;
+		}
+		else {
+			result += '0' + count;
+			result += last_c;
+
+			last_c = input[i];
+			count = 1;
+		}
+	}
+
+	result += '0' + count;
+	result += last_c;
+	return result;
+}
+
+int main() {
+	std::string input{ "1113122113" };
+	for (int i = 0; i < 50; i++)
+		input = look_and_say(input);
+
+	std::cout << input << ": " << input.size() << '\n';
+}
