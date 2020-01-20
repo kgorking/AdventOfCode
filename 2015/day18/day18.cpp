7a26702427eb89f24b3a0abeac89fa4744938645
Kenneth Gorking <dasapfe@yahoo.dk>
2020-01-20 17:31:52 +0100

    day 18 done

---
+++ 2015/day18/day18.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <array>
+#include "input.h"
+
+/*std::array<std::array<char, 7>, 6> lights{ {
+	{ "##.#.#" },
+	{ "...##." },
+	{ "#....#" },
+	{ "..#..." },
+	{ "#.#..#" },
+	{ "####.#" }
+} };*/
+int constexpr Dimensions = 100;// 6;
+
+struct packed_char {
+	unsigned char state : 6;
+	unsigned char num_neighbours : 2; // pack the neighbour count in the upper 2 bits
+};
+
+packed_char& get_packed_char(int x, int y) {
+	return reinterpret_cast<packed_char&>(lights[y][x]);
+}
+
+bool get_state(int x, int y) {
+	if (x < 0 || y < 0 || x >= Dimensions || y >= Dimensions)
+		return false;
+	return ('#' == get_packed_char(x,y).state);
+}
+
+void set_state(int x, int y, bool on) {
+	if (x < 0 || y < 0 || x > Dimensions || y > Dimensions)
+		return;
+	get_packed_char(x, y).state = on ? '#' : '.';
+}
+
+void set_num_neighbours(int x, int y, int neighbours) {
+	get_packed_char(x, y).num_neighbours = (neighbours > 3) ? 0 : neighbours;
+}
+
+void count_neighbours() {
+	for (int y = 0; y < Dimensions; y++) {
+		for (int x = 0; x < Dimensions; x++) {
+			int const neighbours_on =
+				get_state(x - 1, y - 1) +
+				get_state(x, y - 1) +
+				get_state(x + 1, y - 1) +
+				get_state(x - 1, y) +
+				get_state(x + 1, y) +
+				get_state(x - 1, y + 1) +
+				get_state(x, y + 1) +
+				get_state(x + 1, y + 1);
+			set_num_neighbours(x, y, neighbours_on);
+		}
+	}
+}
+
+void update_state() {
+	for (int y = 0; y < Dimensions; y++) {
+		for (int x = 0; x < Dimensions; x++) {
+			// Dont alter the corners
+			if ((x == 0 || x == Dimensions - 1) && (y == 0 || y == Dimensions - 1))
+				continue;
+
+			auto & pc = get_packed_char(x, y);
+			if (pc.state == '#') {
+				if (pc.num_neighbours < 2)
+					pc.state = '.';
+			}
+			else {
+				if (pc.num_neighbours == 3)
+					pc.state = '#';
+			}
+
+			pc.num_neighbours = 0;
+		}
+	}
+}
+
+void print_lights() {
+	for (int y = 0; y < Dimensions; y++) {
+		for (int x = 0; x < Dimensions; x++) {
+			std::cout << get_packed_char(x, y).state;
+		}
+		std::cout << '\n';
+	}
+}
+
+int count_active_lights() {
+	int total = 0;
+	for (int y = 0; y < Dimensions; y++) {
+		for (int x = 0; x < Dimensions; x++) {
+			total += (get_packed_char(x, y).state == '#');
+		}
+	}
+	return total;
+}
+
+int main() {
+#if 0
+	std::cout << "Initial step\n";
+	print_lights();
+
+	for (int i = 0; i < 5; i++) {
+		count_neighbours();
+		update_state();
+
+		std::cout << "\nAfter " << i+1 << " step\n";
+		print_lights();
+	}
+#else
+	for (int i = 0; i < 100; i++) {
+		count_neighbours();
+		update_state();
+	}
+	std::cout << "Active lights after 100 iterations: " << count_active_lights() << '\n';
+#endif
+}
