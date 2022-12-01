429bf022754ebb7654f5e7fcdf26fe35561ef5f4
Kenneth Gorking <kgorking@outlook.com>
2022-12-01 19:26:26 +0100

    day 1 complete

--- 2022/day1/main.cpp
+++ 2022/day1/main.cpp
@@ -1,12 +1,24 @@
-﻿#include <array>
+﻿#include <vector>
+#include <array>
 #include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <ranges>
 
-constexpr std::array<int, 2000> input{
-#include "input.txt"
+const std::vector<std::vector<int>> input{
+	#include "input.txt"
 };
 
 int main() {
+	auto const reduce = [](auto const& vec) { return std::reduce(vec.begin(), vec.end()); };
+	auto const max = [](int l, int r){ return std::max(l,r); };
+
 	// Part 1
+	std::cout << "Max calories: " << std::transform_reduce(input.begin(), input.end(), 0, max, reduce) << '\n';
+
 
 	// Part 2
+	std::array<int, 3> result;
+	std::ranges::partial_sort_copy(input | std::views::transform(reduce), result, std::greater{});
+	std::cout << "Max 3 calories: " << reduce(result);
 }
