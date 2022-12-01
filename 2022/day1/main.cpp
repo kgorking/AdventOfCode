22e6068266059b56ec3de644ff3074140f5325d0
Kenneth Gorking <kgorking@outlook.com>
2022-12-01 23:44:12 +0100

    Simplified the code

--- 2022/day1/main.cpp
+++ 2022/day1/main.cpp
@@ -10,14 +10,14 @@ const std::vector<std::vector<int>> input{
 };
 
 constexpr auto reduce = [](auto const& vec) { return std::reduce(vec.begin(), vec.end()); };
-constexpr auto max = [](int l, int r){ return std::max(l,r); };
 
 int main() {
-	// Part 1
-	std::cout << std::transform_reduce(input.begin(), input.end(), 0, max, reduce) << '\n';
-
-	// Part 2
 	std::array<int, 3> result;
 	std::ranges::partial_sort_copy(input | std::views::transform(reduce), result, std::greater{});
-	std::cout << reduce(result);
+
+	// Part 1
+	std::cout << std::ranges::max(result) << '\n';
+
+	// Part 2
+	std::cout << reduce(result) << '\n';
 }
