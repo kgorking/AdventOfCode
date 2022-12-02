0bc2ea25e0b1ae8e9fc2602c4d098773cbe77683
Kenneth Gorking <kgorking@outlook.com>
2022-12-02 13:00:23 +0100

    minor cleanup

--- 2022/day2/main.cpp
+++ 2022/day2/main.cpp
@@ -6,15 +6,12 @@ constexpr auto input = std::to_array<pair>({
 	#include "input.txt"
 });
 
-constexpr char matrix_p1[3][3] = {
+constexpr char lookup[6][3] = {
 // rock(1), paper(2), scissor(3)
 	{4, 1, 7}, // rock
 	{8, 5, 2}, // paper
-	{3, 9, 6}  // scissor
-};
+	{3, 9, 6}, // scissor
 
-constexpr char matrix_p2[3][3] = {
-// rock(1), paper(2), scissor(3)
 	{3, 1, 2}, // lose(+0) - rotate right
 	{4, 5, 6}, // draw(+3) - rotate none
 	{8, 9, 7}  // win(+6)  - rotate left
@@ -24,8 +21,8 @@ int main() {
 	int res1 = 0;
 	int res2 = 0;
 	for(pair const p : input) {
-		res1 += matrix_p1[p.b][p.a];
-		res2 += matrix_p2[p.b][p.a];
+		res1 += lookup[0 + p.b][p.a];
+		res2 += lookup[3 + p.b][p.a];
 	}
 
 	// Part 1
