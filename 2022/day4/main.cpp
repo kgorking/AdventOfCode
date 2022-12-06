8ed9d7d81186dc2abf73f861eff11e6943fc3766
Kenneth Gorking <kgorking@outlook.com>
2022-12-06 13:41:18 +0100

    Day 4 done

---
+++ 2022/day4/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <array>
+#include <iostream>
+#include <numeric>
+#include <string_view>
+#include <bitset>
+
+struct range {
+	char first, last;
+
+	[[nodiscard]] constexpr bool contains(range const& other) const { return other.first >= first && other.last <= last; }
+	[[nodiscard]] constexpr bool overlaps(range const& other) const { return first <= other.last && other.first <= last; }
+};
+struct pair {
+	range r1, r2;
+
+	constexpr bool one_contains_other() const { return r1.contains(r2) || r2.contains(r1); }
+	constexpr bool one_overlaps_other() const { return r1.overlaps(r2) || r2.overlaps(r1); }
+};
+
+static constexpr auto input = std::to_array<pair>({
+	#include "input.txt"
+});
+
+int main() {
+	// Part 1
+	static constexpr int num_contained = std::accumulate(input.begin(), input.end(), 0, [](int val, pair const& p) {
+		return val + p.one_contains_other();
+	});
+	std::cout << num_contained << '\n';
+
+	// Part 2
+	static constexpr int num_overlapped = std::accumulate(input.begin(), input.end(), 0, [](int val, pair const& p) {
+		return val + p.one_overlaps_other();
+	});
+	std::cout << num_overlapped << '\n';
+}
