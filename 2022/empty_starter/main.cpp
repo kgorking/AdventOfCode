fc9379e283c87b4a53fe86c04da77b3e0fdc6ef7
Kenneth Gorking <kgorking@outlook.com>
2022-12-16 13:30:40 +0100

    Updated starter project

--- 2022/empty_starter/main.cpp
+++ 2022/empty_starter/main.cpp
@@ -1,12 +1,13 @@
-﻿#include <array>
-#include <iostream>
+﻿import std;
 
-constexpr std::array<int, 2000> input{
+constexpr auto input = std::to_array<std::string_view>({
 #include "input.txt"
-};
+});
 
 int main() {
 	// Part 1
+	std::cout << "Part 1: " << 0 << '\n';
 
 	// Part 2
+	std::cout << "Part 2: " << 0 << '\n';
 }
