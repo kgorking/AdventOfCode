59d02ef621d924da3a59f2f47a2ff6c2ea02bccb
Kenneth Gorking <kgorking@outlook.com>
2022-12-21 11:32:30 +0100

    Day 16, broken

--- 2022/day16/main.cpp
+++ 2022/day16/main.cpp
@@ -1,139 +1,116 @@
 ﻿import std;
 #include "../../common/bits.h"
 
-struct valve {
+struct valve_input {
 	std::string_view name;
 	int flow_rate;
 	std::vector<std::string_view> connections;
 };
 
-const auto input = std::to_array<valve>({
+const auto input = std::to_array<valve_input>({
 #if 1
 #include "input.txt"
 #else
-	{{"AA"}, 0, {"DD", "II", "BB"}}, {{"BB"}, 13, {"CC", "AA"}}, {{"CC"}, 2, {"DD", "BB"}}, {{"DD"}, 20, {"CC", "AA", "EE"}},
-		{{"EE"}, 3, {"FF", "DD"}}, {{"FF"}, 0, {"EE", "GG"}}, {{"GG"}, 0, {"FF", "HH"}}, {{"HH"}, 22, {"GG"}}, {{"II"}, 0, {"AA", "JJ"}}, {
-		{"JJ"}, 21, {
-			"II"
-		}
-	}
+#include "example.txt"
+//#include "testcase1.txt"
+//#include "testcase2.txt"
+//#include "testcase3.txt"
+//#include "testcase4.txt"
 #endif
 });
 
-template <typename T, int N>
-using matrix = std::array<std::array<T, N>, N>;
-template <typename T, int N>
-using adjacency_list_t = std::array<std::vector<T>, N>;
+template <typename T, int N> using matrix_t = std::array<std::array<T, N>, N>;
+template <typename T, int N> using adjacency_list_t = std::array<std::vector<T>, N>;
+
 using adjacency_list = adjacency_list_t<short, input.size()>;
+using matrix = matrix_t<short, input.size()>;
 using valve_set = std::bitset<input.size()>;
 
-adjacency_list build_adjacency_list() {
-	// Convert cave names to indices
+// Create an adjacency matrix for the valves
+matrix build_adjacency_matrix() {
 	std::unordered_map<std::string_view, short> name_index;
-	for (valve const& v : input)
+	for (valve_input const& v : input)
 		name_index[v.name] = static_cast<short>(std::distance(input.data(), &v));
 
-	adjacency_list adj;
-	for (valve const& v : input) {
-		short const index = name_index[v.name];
-		for (auto sv : v.connections)
-			adj[index].push_back(name_index[sv]);
+	matrix adj;
+	for (auto& row : adj)
+		row.fill(100);
+
+	for (valve_input const& v : input) {
+		short const u = name_index[v.name];
+		for (auto sv : v.connections) {
+			short const v = name_index[sv];
+			adj[u][v] = 1;
+			adj[v][u] = 1;
+		}
+	}
+
+	for (std::size_t i = 0; i < adj.size(); i++) {
+		adj[i][i] = 0;
 	}
 
 	return adj;
 }
 
-// Returns a matrix with the shortest paths between all valves,
-// in a snappy O(N^3) instant
+// Returns a matrix with the shortest paths between all valves
 template <typename T, int N>
-matrix<T, N> build_shortest_path_matrix(adjacency_list_t<T, N> const& adj) {
-	matrix<T, N> minutes;
-	// std::memset(&minutes, 99, sizeof(minutes));
+matrix build_shortest_path_matrix() {
+	matrix m = build_adjacency_matrix();
 
-	// Initialize the matrix from the adjacency graph
-#if 0
-	for (std::size_t i = 0; i < adj.size(); i++) {
-		minutes[i].fill(89);
-		for (int j : adj[i])
-			minutes[i][j] = 1;
-	}
-	for (std::size_t d = 0; d < N; d++)
-		minutes[d][d] = 0;
-#else
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			minutes[i][j] = (i != j) * std::numeric_limits<T>::max() / 2;
-		}
-		for (int j : adj[i])
-			minutes[i][j] = 1;
-	}
-#endif
 	// Fill in remaining minutes between valves
 	for (int k = 0; k < N; k++) {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				minutes[i][j] = std::min<T>(minutes[i][j], minutes[i][k] + minutes[k][j]);
+				m[i][j] = std::min<T>(m[i][j], m[i][k] + m[k][j]);
 			}
 		}
 	}
 
-	return minutes;
+	return m;
 }
 
-struct traversal_state {
-	// remaining time
-	int time = 0;
-
-	// current pressure
+template <typename T, int N>
+int find_best_pressure(matrix_t<T, N> const& shortest_paths, valve_set const closed_valves, int const valve, int const time) {
+	// Pressure accumulator
 	int pressure = 0;
 
-	// current valve
-	int valve = 0;
+	// Get the index of the first set bit in the closed valve set
+	int n_valve = kg::find_first_set(closed_valves);
 
-	// valves that have been opened
-	valve_set open_valves = 0;
-};
+	// Keep iterating while I have a valid valve index
+	while (n_valve < closed_valves.size()) {
+		// Calculate the time needed to move to this neighbouring valve,
+		// +1 for the time to open the valve.
+		int const n_time = time - (1 + shortest_paths[valve][n_valve]);
 
-template <typename T, int N>
-traversal_state find_best_pressure(matrix<T, N> const& shortest_paths, valve_set const valves, int start_valve, int time) {
-	// Load up the initial state in the queue
-	std::stack<traversal_state> queue;
-	queue.emplace(time - (input[start_valve].flow_rate > 0), 0, start_valve, valve_set{1} << start_valve);
+		// Check that the time limit is not crossed
+		if (n_time >= 0) {
+			// Prepare a bitset with the newly opened valve set.
+			// This is used to 'open' the bit in the closed valve set
+			valve_set const open_n_valve = valve_set{1} << n_valve;
 
-	// Traverse the valves
-	traversal_state max_state;
-	while (!queue.empty()) {
-		traversal_state state = queue.top();
-		queue.pop();
+			// Calculate the pressure of the remaining valves in the remaining time
+			//vis |= open_valve;
+			int const rec_pressure = find_best_pressure(shortest_paths, closed_valves ^ open_n_valve, n_valve, n_time);
 
-		if (state.pressure > max_state.pressure)
-			max_state = state;
+			// Calculate the total pressure this valve will
+			// produce in the remaining minutes it is open
+			int const n_pressure = input[n_valve].flow_rate * n_time;
 
-		auto const closed_valves = valves ^ state.open_valves;
-		int valve = kg::find_first_set(closed_valves);
-		while (valve < closed_valves.size()) {
-			auto neighbour = state;
-			neighbour.time -= 1 + shortest_paths[state.valve][valve];
-			if (neighbour.time >= 0) {
-				neighbour.pressure += input[valve].flow_rate * neighbour.time;
-				neighbour.valve = valve;
-				neighbour.open_valves.set(valve);
-				queue.push(neighbour);
-			}
-
-			valve = kg::bit_position_left(closed_valves, valve);
+			// Update the maximum pressure found
+			pressure = std::max(pressure, n_pressure + rec_pressure);
 		}
+
+		// Skip to the next valve index in the bitset
+		n_valve = kg::bit_position_left(closed_valves, n_valve);
 	}
 
-	return max_state;
+	return pressure;
 }
 
 int main() {
-	// Create an adjacency list for the valves
-	auto const adj_list = build_adjacency_list();
-
 	// Create the shortest-path minute-cost matrix
-	auto const shortest_paths = build_shortest_path_matrix(adj_list);
+	auto const shortest_paths = build_shortest_path_matrix<short, input.size()>();
 
 	// The 'interresting' valves
 	valve_set valves;
@@ -142,7 +119,7 @@ int main() {
 			valves.set(i);
 	}
 
-	auto const max_pressure_30 = find_best_pressure(shortest_paths, valves, 0, 30);
+	int const max_pressure_30 = find_best_pressure(shortest_paths, valves, 0, 30);
 
 	// Part 1
 	// > 1836
@@ -151,7 +128,7 @@ int main() {
 	// ! 2285
 	// < 2296
 	// < 2484
-	std::cout << "Part 1: " << max_pressure_30.pressure << '\n';
+	std::cout << "Part 1: " << max_pressure_30 << '\n';
 
 	// Part 2
 	std::cout << "Part 2: " << 0 << '\n';
