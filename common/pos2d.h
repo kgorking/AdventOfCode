363a70faa9bbdc01df46b69f23b8199929d82f32
Kenneth Gorking <kgorking@outlook.com>
2023-01-18 15:59:55 +0100

    Merged code

--- common/pos2d.h
+++ common/pos2d.h
@@ -1,11 +1,23 @@
 export namespace kg {
 
-template <typename T>
+template <typename T = int>
 struct pos2d {
 	T x = 0, y = 0;
 
-	pos2d operator+(pos2d p) const {
+	[[nodiscard]] constexpr pos2d operator+(pos2d const p) const {
 		return {x + p.x, y + p.y};
+	}
+
+	// Move this point towards 'p'
+	[[nodiscard]] constexpr bool follow(pos2d const p) {
+		int const dx = (p.x - x);
+		int const dy = (p.y - y);
+
+		// branchless
+		bool const b = (std::abs(dx) | std::abs(dy)) > 1;
+		x += b * ((dx > 0) - (dx < 0));
+		y += b * ((dy > 0) - (dy < 0));
+		return b;
 	}
 
 	auto operator<=>(pos2d const&) const noexcept = default;
