d8aa280067845ab2c947b16051e4917d182af869
Kenneth Gorking <kgorking@outlook.com>
2023-12-08 10:32:24 +0100

    format

--- 2023/day08/main.cpp
+++ 2023/day08/main.cpp
@@ -1,6 +1,6 @@
 ﻿import aoc;
 
-using node_map = std::map<std::string_view, std::array<std::string_view,2>>;
+using node_map = std::map<std::string_view, std::array<std::string_view, 2>>;
 
 struct input_t {
 	std::string_view directions;
@@ -21,9 +21,7 @@ auto solve(bool p2) {
 	};
 
 	auto is_start_node = [p2](auto n) { return (!p2) ? n == "AAA" : n.back() == 'A'; };
-	auto start_nodes = input.nodes 
-		| std::views::keys
-		| std::views::filter(is_start_node);
+	auto start_nodes = input.nodes | std::views::keys | std::views::filter(is_start_node);
 
 	auto const lcm = [](auto... a) { return std::lcm(a...); };
 	return std::transform_reduce(start_nodes.begin(), start_nodes.end(), std::size_t{1}, lcm, distance);
