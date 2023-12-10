93b4eed2df69e6e46c4ef0095d80e10795dea703
Kenneth Gorking <kgorking@outlook.com>
2023-12-10 23:29:56 +0100

    updated future days

--- 2023/day18/main.cpp
+++ 2023/day18/main.cpp
@@ -4,10 +4,12 @@ constexpr auto input = std::to_array<std::string_view>({
 #include "input.txt"
 });
 
-int main() {
-	// Part 1
-	std::cout << "Part 1: " << 0 << '\n';
+auto solve() {
+	return std::make_pair(0, 0);
+}
 
-	// Part 2
-	std::cout << "Part 2: " << 0 << '\n';
+int main() {
+	auto const [p1, p2] = solve();
+	std::cout << "Part 1: " << p1 << '\n';
+	std::cout << "Part 2: " << p2 << '\n';
 }
