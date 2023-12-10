f88ec0c7a73500404f0d0e8cf59ff871f86b056d
Kenneth Gorking <kgorking@outlook.com>
2023-12-10 22:37:17 +0100

    Day 10 done

--- 2023/day10/main.cpp
+++ 2023/day10/main.cpp
@@ -1,6 +1,6 @@
 ﻿import aoc;
 
-auto input = std::to_array<std::string_view>({
+constexpr auto input = std::to_array<std::string_view>({
 #include "input.txt"
 });
 
@@ -8,10 +8,6 @@ using namespace std::string_view_literals;
 using pos2d = kg::pos2d<>;
 
 enum dir { none, up, down, left, right };
-
-int follow_pipe(int x, int y) {
-	return 0;
-}
 
 pos2d find_start_position() {
 	int x = 0, y = 0;
@@ -36,20 +32,14 @@ pos2d advance_position(pos2d p, dir d) {
 dir dir_from_tile(pos2d p, dir prev_dir) {
 	char const tile = input[p.y][p.x];
 	switch (tile) {
-	case '|':
-		return (prev_dir == down) ? down : up;
-	case '-':
-		return (prev_dir == right) ? right : left;
-	case 'L':
-		return (prev_dir == down) ? right : up;
-	case 'J':
-		return (prev_dir == down) ? left : up;
-	case '7':
-		return (prev_dir == up) ? left : down;
-	case 'F':
-		return (prev_dir == up) ? right : down;
+	case '|': return (prev_dir == down) ? down : up;
+	case '-': return (prev_dir == right) ? right : left;
+	case 'L': return (prev_dir == down) ? right : up;
+	case 'J': return (prev_dir == down) ? left : up;
+	case '7': return (prev_dir == up) ? left : down;
+	case 'F': return (prev_dir == up) ? right : down;
 	case 'S':
-		if (p.y - 1 > 0 && "|7F"sv.contains(input[p.y - 1][p.x]) && prev_dir != up)
+		if (p.y - 1 > 0 && "|7F"sv.contains(input[p.y - 1][p.x]) && prev_dir != up) 
 			return up;
 		if (p.x + 1 < input.size() && "|JL"sv.contains(input[p.y + 1][p.x]) && prev_dir != down)
 			return down;
@@ -62,46 +52,32 @@ dir dir_from_tile(pos2d p, dir prev_dir) {
 	}
 }
 
-int test1() {
+auto solve() {
 	pos2d pos = find_start_position();
 	dir next_dir = dir_from_tile(pos, none);
 
-	int counter = 0;
+	std::vector<pos2d> verts;
+	verts.push_back(pos);
 
+	int area = 0;
 	do {
-		counter++;
 		pos = advance_position(pos, next_dir);
 		next_dir = dir_from_tile(pos, next_dir);
+
+		area += (verts.back().x + pos.x) * (verts.back().y - pos.y);
+
+		verts.push_back(pos);
 	} while (!is_start_position(pos));
 
-	return counter / 2;
-}
+	area = std::abs(area) / 2;
+	auto const max_dist = std::ssize(verts) / 2;
 
-int test2() {
-	pos2d pos = find_start_position();
-	dir next_dir = dir_from_tile(pos, none);
-
-	int counter = 0;
-
-	do {
-		//input[pos.y][pos.x] = counter++;
-		pos = advance_position(pos, next_dir);
-		next_dir = dir_from_tile(pos, next_dir);
-	} while (!is_start_position(pos));
-
-	return counter / 2;
-}
-
-int part1() {
-	return 0;
-}
-int part2() {
-	return 0;
+	// Pick's theorem
+	return std::make_pair(max_dist, area - max_dist + 1);
 }
 
 int main() {
-	 std::cout << "Test 1: " << test1() << '\n';
-	 std::cout << "Test 2: " << test2() << '\n';
-	// std::cout << "Part 1: " << 0 << '\n';
-	// std::cout << "Part 2: " << 0 << '\n';
+	auto const [p1, p2] = solve();
+	std::cout << "Part 1: " << p1 << '\n';
+	std::cout << "Part 2: " << p2 << '\n';
 }
