1760f64f2f67d6a70a951d2bd3a38cf2279891f6
Kenneth Gorking <kgorking@outlook.com>
2023-12-16 16:07:47 +0100

    Day 16 part 2 done

--- 2023/day16/impl.cpp
+++ 2023/day16/impl.cpp
@@ -5,18 +5,18 @@ using pos2d = kg::pos2d<>;
 enum direction { none, up, down, left, right };
 constexpr auto offsets = std::to_array<pos2d>({{0, 0}, {0, -1}, {0, +1}, {-1, 0}, {+1, 0}});
 
-// Convert a tile and direction to a new direction
-constexpr auto lookup = kg::matrix_t<int, 5, 5>{{/* '.' */ {none, up, down, left, right},
-												 /* '/' */ {none, right, left, down, up},
-												 /* '\' */ {none, left, right, up, down},
-												 /* '|' */ {none, up, down, up | down << 4, up | down << 4},
-												 /* '-' */ {none, left | right << 4, left | right << 4, left, right}}};
+// Convert a tile and direction to a new direction         none,	up,					down,				left,				right
+constexpr auto lookup = kg::matrix_t<int, 5, 5>{{/* '.' */ {none,	up,					down,				left,				right},
+												 /* '/' */ {none,	right,				left,				down,				up},
+												 /* '\' */ {none,	left,				right,				up,					down},
+												 /* '|' */ {none,	up,					down,				up | down << 4,		up | down << 4},
+												 /* '-' */ {none,	left | right << 4,	left | right << 4,	left,				right}}};
 
 auto to_index = [](char c) { return std::string_view{R"(./\|-)"}.find(c); };
 auto to_bit = [](char d) { return 1 << (d - 1); };
 auto advance_position = [](pos2d p, char d) { return p + offsets[d]; };
 
-constexpr auto part1(auto const& input) {
+constexpr int count_energized(auto const& input, pos2d start, direction dir) {
 	auto energized = kg::make_dmatrix<char>(input.size(), input[0].size());
 	auto stack = std::stack<std::pair<pos2d, char>>{};
 
@@ -26,7 +26,7 @@ constexpr auto part1(auto const& input) {
 			stack.push({p, dir});
 	};
 
-	push_stack({0, 0}, right);
+	push_stack(start, dir);
 
 	while (!stack.empty()) {
 		auto const [pos, dir] = stack.top();
@@ -50,6 +50,25 @@ constexpr auto part1(auto const& input) {
 	return sum;
 }
 
+constexpr auto part1(auto const& input) {
+	return count_energized(input, {0,0}, right);
+}
+
 constexpr auto part2(auto const& input) {
-	return 0;
+	// Build a vector of starting positions
+	std::vector<std::pair<pos2d, direction>> starts;
+	starts.reserve(2 * (input.size() + input[0].size()));
+
+	for (char x = 0; x < input[0].size(); x++) {
+		starts.emplace_back(pos2d{x, 0}, down);
+		starts.emplace_back(pos2d{x, (char)input.size()-1}, up);
+	}
+	for (char y = 0; y < input.size(); y++) {
+		starts.emplace_back(pos2d{0, y}, right);
+		starts.emplace_back(pos2d{(char)input[0].size() - 1, y}, left);
+	}
+
+	auto count = [&input](auto pair) { return count_energized(input, pair.first, pair.second); };
+	auto max = [](int a, int b) { return (a > b) ? a : b; };
+	return std::transform_reduce(std::execution::par_unseq, starts.begin(), starts.end(), 0, max, count);
 }
