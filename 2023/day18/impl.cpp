66aba55fca0eb9cac1d317016a2004d021a0dbce
Kenneth Gorking <kgorking@outlook.com>
2023-12-18 11:07:16 +0100

    Cleanup

--- 2023/day18/impl.cpp
+++ 2023/day18/impl.cpp
@@ -11,44 +11,30 @@ struct dig {
 	int color;
 };
 
-struct edge {
-	pos a, b;
-};
-
-i64 area_incl_edges(std::vector<edge> const& edges) {
+constexpr auto part1(auto const& input) { // array of 'dig'
 	i64 area = 0;
 	i64 edge_length = 0;
-	for (auto const [last_pos, pos] : edges) {
-		area += (last_pos.x + pos.x) * (last_pos.y - pos.y);
-		edge_length += last_pos.steps_to(pos);
+	pos prev{0, 0};
+	for (auto d : input) {
+		pos const next = prev + offsets[d.dir] * d.count;
+		area += (prev.x + next.x) * (prev.y - next.y);
+		edge_length += d.count;
+		prev = next;
 	}
 	return 1 + (edge_length / 2) + (std::abs(area) / 2);
 }
 
-constexpr auto part1(auto const& input) { // array of 'dig'
-	// Create polygon
-	std::vector<edge> poly;
-	pos prev{0, 0};
-	for (auto d : input) {
-		pos const next = prev + offsets[d.dir] * d.count;
-		poly.emplace_back(prev, next);
-		prev = next;
-	}
-
-	return area_incl_edges(poly);
-}
-
 constexpr auto part2(auto const& input) {
-	// Create polygon
-	std::vector<edge> poly;
+	i64 area = 0;
+	i64 edge_length = 0;
 	pos prev{0, 0};
 	for (auto d : input) {
 		int const count = (d.color & 0xFFFFF0) >> 4;
 		int const dir = d.color & 0x00000F;
 		pos const next = prev + offsets[dir] * count;
-		poly.emplace_back(prev, next);
+		area += (prev.x + next.x) * (prev.y - next.y);
+		edge_length += count;
 		prev = next;
 	}
-
-	return area_incl_edges(poly);
+	return 1 + (edge_length / 2) + (std::abs(area) / 2);
 }
