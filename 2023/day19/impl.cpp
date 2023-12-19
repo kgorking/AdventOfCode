a8ea5d5b6287334f926d866483164825cc24a4dc
Kenneth Gorking <kgorking@outlook.com>
2023-12-19 07:52:10 +0100

    Day 19 part 1 done

--- 2023/day19/impl.cpp
+++ 2023/day19/impl.cpp
@@ -1,13 +1,35 @@
 import aoc;
 
+#define RULE +[](int x, int m, int a, int s) -> std::string_view 
+
+using workflow_t = std::unordered_map<std::string_view, std::string_view(*)(int,int,int,int)>;
+struct rating_t { int x, m, a, s; };
+
+struct input_t {
+	workflow_t workflows;
+	std::vector<rating_t> ratings;
+};
+
 constexpr auto part1(auto const& input) {
-	return 0;
+	int sum = 0;
+	for (auto const [x, m, a, s] : input.ratings) {
+		std::string_view name = "in";
+		while (true) {
+			name = input.workflows.at(name)(x, m, a, s);
+
+			if (name[0] == 'A') {
+				sum += (x + m + a + s);
+				break;
+			} else if (name[0] == 'R') {
+				break;
+			}
+		}
+		
+	}
+
+	return sum;
 }
 
 constexpr auto part2(auto const& input) {
 	return 0;
-}
-
-auto solve(auto const& input) {
-	return std::make_pair(part1(input), part2(input));
 }
