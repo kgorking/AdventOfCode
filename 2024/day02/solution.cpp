5019a938d3a6dd575390be07731a101beacc8a3e
Kenneth Gorking <kgorking@outlook.com>
2024-12-01 14:09:32 +0100

    Set up day02

--- 2024/day02/solution.cpp
+++ 2024/day02/solution.cpp
@@ -1,18 +1,14 @@
 export module aoc.day02;
+import aoc;
 import std;
 
-namespace AOC_NS {
+export constexpr auto expected_sample = std::make_pair(0, 0);
+export constexpr auto expected_input = std::make_pair(0, 0);
 
-constexpr auto expected_sample = std::make_pair(0, 0);
-constexpr auto expected_input = std::make_pair(0,0);
-constexpr auto sample_input = 0;
-constexpr auto actual_input = 0;
-
-auto part1(auto&& input) {
+export auto part1(auto&& input) {
 	return 0;
 }
 
-auto part2(auto&& input) {
+export auto part2(auto&& input) {
 	return 0;
 }
-} // namespace AOC_NS
