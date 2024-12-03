73c91a78b084337e2db7ab93c7868c70d7e11fd5
Kenneth Gorking <kgorking@outlook.com>
2024-12-03 01:41:47 +0100

    Ready for day 3

---
+++ 2024/day03/solution.cpp
@@ -0,0 +1,14 @@
+export module aoc.day03;
+import aoc;
+import std;
+
+export constexpr auto expected_sample = std::make_pair(0, 0);
+export constexpr auto expected_input = std::make_pair(0, 0);
+
+export auto part1(auto&& input) {
+	return 0;
+}
+
+export auto part2(auto&& input) {
+	return 0;
+}
