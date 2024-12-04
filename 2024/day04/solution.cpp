a897d5aaf7bf0d825c00b417114549c7dd99bde7
Kenneth Gorking <kgorking@outlook.com>
2024-12-04 21:13:16 +0100

    Cleanup

--- 2024/day04/solution.cpp
+++ 2024/day04/solution.cpp
@@ -10,16 +10,14 @@ template <bool X, int N>
 int xmas_count(std::array<std::string_view, N> input, char const c, std::span<const kg::pos2di> directions, std::span<const int, 3> offsets) {
 	return kg::sum(kg::views::indexed_transform(input, [&](int sy, std::string_view line) {
 		return kg::sum(kg::views::indexed_transform(line, [&](int sx, char v) {
-			return (c != v) ? 0 : kg::sum(directions | std::views::transform([&](kg::pos2di dir) {
-					// Check if a word exists in the direction
-					return std::ranges::equal(std::string_view{"MAS"}, offsets | std::views::transform([&](int offset) {
-						// Convert an offset and direction to a char
-						kg::pos2di const p = kg::pos2di(sx, sy) + (dir * offset);
-						return (p.y >= 0 && p.y < input.size() && p.x >= 0 && p.x < input[p.y].size()) ? input[p.y][p.x] : '\0';
+				return (c != v) ? 0 : kg::sum(directions | std::views::transform([&](kg::pos2di dir) {
+						return std::ranges::equal(std::string_view{"MAS"}, offsets | std::views::transform([&](int offset) {
+							kg::pos2di const p = kg::pos2di(sx, sy) + (dir * offset);
+							return (p.y >= 0 && p.y < input.size() && p.x >= 0 && p.x < input[p.y].size()) ? input[p.y][p.x] : '\0';
+						}));
 					}));
-				}));
-		})
-		| std::views::transform([](int v) { return X ? (v == 2) : v; }));
+			})
+			| std::views::transform([](int v) { return X ? (v == 2) : v; }));
 	}));
 }
 
--- aoc/aoc-ranges.ixx
+++ aoc/aoc-ranges.ixx
@@ -2,6 +2,6 @@ export module aoc:ranges;
 import std;
 
 export namespace kg {
-	constexpr auto sum = std::bind_back(std::ranges::fold_left, 0, std::plus{});
+	constexpr auto sum = std::bind_back(std::ranges::fold_left, 0ll, std::plus{});
 }
 
