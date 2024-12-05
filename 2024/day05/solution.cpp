5999ee19e515908c356d1b3fe52ea0c2db494ba9
Kenneth Gorking <kgorking@outlook.com>
2024-12-05 18:07:59 +0100

    It's just a goddamn sorting function

--- 2024/day05/solution.cpp
+++ 2024/day05/solution.cpp
@@ -9,63 +9,29 @@ export constexpr auto expected_input = std::make_pair(4662, 5900);
 auto solution(auto&& input, bool fix) {
 	auto [rules, all_updates] = input;
 
-	std::multimap<int, int> ruleset;
+	std::unordered_map<int, std::unordered_set<int>> order;
 	for (auto [before, after] : rules) {
-		ruleset.insert({ after, before }); // NOTE: switched
+		order[before].insert(after);
 	}
 
-	// Accumulate the valid/fixed values
-	int num_valid[2] = { 0, 0 };
-	bool value_fixed = false;
+	auto sort_fn = [&order](int l, int r) {
+		return order[l].contains(r);
+	};
 
-	for (std::vector<int>& updates : all_updates) {
-		value_fixed = false;
-		int count_invalid = 0;
-		int mid_value = updates[updates.size() / 2];
-
-		do {
-			// Check how many invalid pages are present
-			std::map<int, int> checks;
-			for (int i = 0; i < updates.size(); i++) {
-				// Set the current pages count to the number of pages after it
-				int const page = updates[i];
-				checks[page] = updates.size() - i - 1;
-
-				// Decrement former pages counts if they need to be located before the current page
-				for (auto [i, end] = ruleset.equal_range(page); i != end; i++) {
-					if (checks.contains(i->second))
-						checks[i->second] -= 1;
-				}
-			}
-
-			count_invalid = kg::sum(checks | std::views::values);
-			if (count_invalid > 0) {
-				if (!fix)
-					mid_value = 0;
-				else {
-					value_fixed = true;
-					for (auto [page, bad_count] : checks) {
-						if (0 == bad_count)
-							continue;
-						// The remaining count for the bad page is
-						// used to move it forward in the list of pages.
-						auto it = std::ranges::find(updates, page);
-						std::ranges::swap(*it, *(it + bad_count));
-
-						// Update the mid-value, as it might have changed
-						mid_value = updates[updates.size() / 2];
-
-						// Only fix one page at the time
-						break;
-					}
-				}
-			}
-		} while (count_invalid && fix);
-
-		num_valid[value_fixed] += mid_value;
+	if (!fix) {
+		return kg::sum(all_updates
+			| std::views::filter([&](auto const& v) {
+				return std::ranges::is_sorted(v, sort_fn); })
+			| std::views::transform([](auto const& v) {
+				return v[v.size() / 2];}));
+	} else {
+		return kg::sum(all_updates
+			| std::views::filter([&](auto const& v) {
+				return !std::ranges::is_sorted(v, sort_fn); })
+			| std::views::transform([&](auto v) {
+				std::ranges::sort(v, sort_fn);
+				return v[v.size() / 2];}));
 	}
-
-	return num_valid[fix];
 }
 
 export auto part1(auto&& input) { return solution(input, false); }
