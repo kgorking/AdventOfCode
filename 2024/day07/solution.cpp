cb7be8988e37e0c53ded016a9410a44d165179e7
Kenneth Gorking <kgorking@outlook.com>
2024-12-09 13:42:05 +0100

    Rady for day 07

---
+++ 2024/day07/solution.cpp
@@ -0,0 +1,17 @@
+export module aoc.day07;
+import aoc;
+import std;
+
+// Holds the expected values for sample + input. Used in unit tests.
+export constexpr auto expected_sample = std::make_pair(0, 0);
+export constexpr auto expected_input = std::make_pair(0, 0);
+
+using input_t = int;//std::span<std::string_view const>;
+
+export auto part1(input_t input) {
+	return 0;
+}
+
+export auto part2(input_t input) {
+	return 0;
+}
