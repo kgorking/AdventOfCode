d108d77727690ea42f70405b0087d37f22ad447e
Kenneth Gorking <kgorking@outlook.com>
2024-12-10 16:59:27 +0100

    Cleanup

--- 2024/day04/solution.cpp
+++ 2024/day04/solution.cpp
@@ -6,25 +6,25 @@ import std;
 export constexpr auto expected_sample = std::make_pair(18, 9);
 export constexpr auto expected_input = std::make_pair(2406, 1807);
 
-template <bool X, int N>
-int xmas_count(std::array<std::string_view, N> input, char const c, std::span<const kg::pos2di> directions, std::span<const int, 3> offsets) {
-	return kg::sum(kg::views::indexed_transform(input, [&](int sy, std::string_view line) {
-		return kg::sum(kg::views::indexed_transform(line, [&](int sx, char v) {
-			return (c != v) ? 0 : kg::sum(directions | std::views::transform([&](kg::pos2di dir) {
-				auto word_in_dir = offsets | std::views::transform([&](int offset) {
-					kg::pos2di const p = kg::pos2di(sx, sy) + (dir * offset);
-					return (p.y >= 0 && p.y < input.size() && p.x >= 0 && p.x < input[p.y].size()) ? input[p.y][p.x] : '\0';
-				});
-				return std::ranges::equal(std::string_view { "MAS" }, word_in_dir);
-			}));
-		}) | std::views::transform([](int v) { return X ? (v == 2) : v; }));
-	}));
-}
+constexpr auto directions
+	= std::to_array<kg::pos2di>({ { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 }, { 1, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 } });
 
 export auto part1(auto const& input) {
-	return xmas_count<false>(input, 'X',
-		std::to_array<kg::pos2di>({ { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 }, { 1, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 } }),
-		std::array { 1, 2, 3 });
+	auto count_xmas_in_all_directions
+		= input
+		| kg::views::coord2d
+		| kg::views::filter_eq('X', kg::select<0>)
+		| std::views::values
+		| std::views::transform([&](kg::pos2di p) {
+				return kg::sum(directions | std::views::transform([&](kg::pos2di dir) {
+					auto word_in_dir = std::views::iota(1,4) | std::views::transform([&](int offset) {
+						kg::pos2di const w = p + (dir * offset);
+						return (w.y >= 0 && w.y < input.size() && w.x >= 0 && w.x < input[w.y].size()) ? input[w.y][w.x] : '\0';
+					});
+					return std::ranges::equal(std::string_view { "MAS" }, word_in_dir);
+				}));
+			});
+	return kg::sum(count_xmas_in_all_directions);
 }
 
 export auto part2(auto const& input) {
