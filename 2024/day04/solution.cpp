956a9badb58780464dfc3d09a33af760210f11b8
Kenneth Gorking <kgorking@outlook.com>
2024-12-10 21:33:59 +0100

    Fixed day 04 not working in release mode

--- 2024/day04/solution.cpp
+++ 2024/day04/solution.cpp
@@ -17,10 +17,11 @@ export auto part1(auto const& input) {
 		| std::views::values
 		| std::views::transform([&](kg::pos2di p) {
 				return kg::sum(directions | std::views::transform([&](kg::pos2di dir) {
-					auto word_in_dir = std::views::iota(1,4) | std::views::transform([&](int offset) {
+					auto offset_to_char_in_input = [&](int offset) {
 						kg::pos2di const w = p + (dir * offset);
 						return kg::at(input, w, '\0');
-					});
+					};
+					auto word_in_dir = std::views::iota(1,4) | std::views::transform(offset_to_char_in_input);
 					return std::ranges::equal(std::string_view { "MAS" }, word_in_dir);
 				}));
 			});
@@ -31,7 +32,7 @@ export auto part2(auto const& input) {
 	return kg::sum(input
 		| kg::views::matrix<3, 3>
 		| kg::views::filter_eq('A', kg::index<1, 1>{})
-		| std::views::transform([](auto const& m) {
+		| std::views::transform([](kg::matrix_t<char,3,3> const& m) {
 			return
 				6 == std::abs(m[0][0] - m[2][2]) &&
 				6 == std::abs(m[0][2] - m[2][0]); })
