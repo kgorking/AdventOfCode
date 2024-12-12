7eb3d734ffd89f30cc21e7b2e879b10248ee2556
Kenneth Gorking <kgorking@outlook.com>
2024-12-12 18:53:20 +0100

    Update to use grid/visited

--- 2024/day10/solution.cpp
+++ 2024/day10/solution.cpp
@@ -12,47 +12,47 @@ bool good_neighbour(char me, char n) {
 	return 1 == (n - me);
 };
 
-void traverse_all_paths(auto const& terrain, kg::pos2di const start_pos, auto&& is_at_end, auto&& valid_neighbour) {
-	std::vector<kg::pos2di> queue;
-	queue.push_back(start_pos);
+template<class T>
+void traverse_all_paths(kg::grid<T> const& grid, kg::pos2di const start_pos, auto&& is_at_end, auto&& valid_neighbour) {
+	std::vector<kg::pos2di> stack;
+	stack.push_back(start_pos);
 
-	while (!queue.empty()) {
-		auto const p = queue.back();
-		queue.pop_back();
+	while (!stack.empty()) {
+		auto const p = stack.back();
+		stack.pop_back();
 
 		if (!is_at_end(p)) {
 			auto neighbours = directions
 				| std::views::transform([&](auto d) { return p + d; })
-				| std::views::filter([&](auto n) { return kg::is_valid(terrain, n) && 1 == terrain[n.y][n.x] - terrain[p.y][p.x]; });
-			//queue.insert_range(queue.end(), neighbours); // too slow
+				| std::views::filter([&](auto n) { return 1 == grid[n] - grid[p]; });
+			//stack.insert_range(stack.end(), neighbours); // too slow
 			for (auto n : neighbours)
-				queue.push_back(n);
+				stack.push_back(n);
 		}
 	}
 }
 
 export auto part1(auto&& input) {
-	auto visited = kg::make_dmatrix<short>(input.size(), input.size());
+	auto grid = kg::grid { input, -1 };
+	auto visited = kg::visited<short>(grid);
 
-	int curr_loop = 1; // use this so I don't have to clear the 'visited' matrix
+	short curr_loop = 1; // use this so I don't have to clear the 'visited' matrix
 	int num_paths = 0;
 
 	auto at_end = [&](kg::pos2di p) {
-		if (visited[p.y][p.x] == curr_loop)
+		if (visited.test_or_set(p, curr_loop))
 			return true;
-		visited[p.y][p.x] = curr_loop;
 
-		char const val = kg::at(input, p, char(0));
+		char const val = grid[p];
 		num_paths += (9 == val);
 		return (9 == val);
 	};
 
-	auto calc_path_scores = input
-		| kg::views::with_coord2d // -> {key: char, value: kg::pos2di}
+	auto calc_path_scores = grid.values_and_coords()
 		| kg::views::filter_eq(0, kg::select<0>)
 		| std::views::values
 		| std::views::transform([&](kg::pos2di p) {
-				::traverse_all_paths(input, p, at_end, good_neighbour);
+				::traverse_all_paths(grid, p, at_end, good_neighbour);
 				curr_loop += 1;
 				return std::exchange(num_paths, 0);
 			});
@@ -61,19 +61,20 @@ export auto part1(auto&& input) {
 }
 
 export auto part2(auto&& input) {
+	auto grid = kg::grid { input, -1 };
+
 	auto traverse = [&](this auto const& self, kg::pos2di p) -> int {
-		if (9 == input[p.y][p.x])
+		if (9 == grid[p])
 			return 1;
 
 		auto scores = directions
 			| std::views::transform([&](auto d) { return p + d; })
-			| std::views::filter([&](auto n) { return kg::is_valid(input, n) && 1 == input[n.y][n.x] - input[p.y][p.x]; })
+			| std::views::filter([&](auto n) { return 1 == grid[n] - grid[p]; })
 			| std::views::transform([&](auto n) { return self(n); });
 		return kg::sum(scores);
 	};
 
-	auto calc_path_scores = input
-		| kg::views::with_coord2d // -> {key: char, value: kg::pos2di}
+	auto calc_path_scores = grid.values_and_coords()
 		| kg::views::filter_eq(0, kg::select<0>)
 		| std::views::values
 		| std::views::transform(traverse);
