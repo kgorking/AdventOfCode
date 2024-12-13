9076853dad2c048eff840083efe103c39459dac0
Kenneth Gorking <kgorking@outlook.com>
2024-12-13 12:10:57 +0100

    Cleanup

--- 2024/day13/solution.cpp
+++ 2024/day13/solution.cpp
@@ -10,16 +10,16 @@ template<typename i64 = std::int64_t>
 std::pair<i64, i64> solve(i64 a1, i64 b1, i64 c1, i64 a2, i64 b2, i64 c2) {
 	i64 y = (a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1);
 	i64 x = (b2 * c1 - b1 * c2) / (a1 * b2 - a2 * b1);
-	return std::pair { x, y };
+	return { x, y };
 }
 
 export auto part1(auto&& input) {
 	std::int64_t sum = 0;
-	for (auto eq : input) {
-		auto [a, b, c, d, e, f] = eq;
+	for (auto const& eq : input) {
+		auto [a1, b1, c1, a2, b2, c2] = eq;
 
-		auto [x, y] = solve(a, b, c, d, e, f);
-		bool const valid_solution = (a * x + b * y == c) && (d * x + e * y == f);
+		auto [x, y] = solve(a1, b1, c1, a2, b2, c2);
+		bool const valid_solution = (a1 * x + b1 * y == c1) && (a2 * x + b2 * y == c2);
 		if (valid_solution && x >= 0 && y >= 0 && x <= 100 && y <= 100) {
 			sum += x * 3 + y;
 		}
@@ -30,14 +30,14 @@ export auto part1(auto&& input) {
 
 export constexpr auto part2(auto&& input) {
 	std::int64_t sum = 0;
-	for (auto eq : input) {
-		auto [a, b, c, d, e, f] = eq;
+	for (auto const& eq : input) {
+		auto [a1, b1, c1, a2, b2, c2] = eq;
 
-		c += 10000000000000ll;
-		f += 10000000000000ll;
+		c1 += 10000000000000ll;
+		c2 += 10000000000000ll;
 
-		auto [x, y] = solve(a, b, c, d, e, f);
-		bool const valid_solution = (a * x + b * y == c) && (d * x + e * y == f);
+		auto [x, y] = solve(a1, b1, c1, a2, b2, c2);
+		bool const valid_solution = (a1 * x + b1 * y == c1) && (a2 * x + b2 * y == c2);
 		if (valid_solution && x >= 0 && y >= 0) {
 			sum += x * 3 + y;
 		}
