a9b38454d98036e05153e5252a8d02330377c4c2
Kenneth Gorking <kgorking@outlook.com>
2024-12-14 20:31:29 +0100

    Cleanup

--- 2024/day14/solution.cpp
+++ 2024/day14/solution.cpp
@@ -16,47 +16,44 @@ export auto part1(auto&& input) {
 	kg::pos2di const dim { w, h };
 	kg::pos2di const hdim { w / 2, h / 2 };
 
-	std::array<int, 5> counts;
+	std::array<int, 4> counts;
 	counts.fill(0);
 
 	for (auto [p, v] : input) {
 		p += v * 100;
 		p %= dim;
-		if (p.x == hdim.x || p.y == hdim.y)
-			continue;
 
-		counts[quadrant(p - hdim)] += 1;
+		if (p.x != hdim.x && p.y != hdim.y)
+			counts[quadrant(p - hdim)] += 1;
 	}
 
 	return counts[0] * counts[1] * counts[2] * counts[3];
 }
 
 export constexpr auto part2(auto&& input) {
-	if (input.size() == 12)
+	if (input.size() == 12) // doesn't work with sample data
 		return 0;
+
 	kg::pos2di constexpr dim { 101, 103 };
 	kg::pos2di constexpr hdim { dim.x / 2, dim.y / 2 };
 
 	auto robots = input;
+	std::array<int, 4> counts;
 
 	for (int const second : std::views::iota(1)) {
-		std::array<int, 4> counts;
 		counts.fill(0);
 
 		for (auto& [p, v] : robots) {
 			p += v;
 			p %= dim;
 
-			if (p.x == hdim.x || p.y == hdim.y)
-				continue;
-
-			int const quad = quadrant(p - hdim);
-			counts[quad] += 1;
+			if (p.x != hdim.x && p.y != hdim.y)
+				counts[quadrant(p - hdim)] += 1;
 		}
 
 		if (counts[1] + counts[3] > 2 * (counts[0] + counts[2])) {
 			std::ranges::sort(robots, [](auto const& l, auto const& r) { return l[0].y < r[0].y; });
-			for (int y = 0; y < 103; y++) {
+			for (int y = 0; y < dim.y; y++) {
 				int orig_y = y;
 				while (robots[y][0].y == robots[orig_y][0].y) {
 					y += 1;
