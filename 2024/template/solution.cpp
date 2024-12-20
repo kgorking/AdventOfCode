4c4e6446bb7093aeaa8ae5a10bf4a3e63542bc93
Kenneth Gorking <kgorking@outlook.com>
2024-12-20 15:05:34 +0100

    Only take const input

--- 2024/template/solution.cpp
+++ 2024/template/solution.cpp
@@ -7,10 +7,10 @@ export constexpr auto expected_sample = std::make_pair(0, 0);
 export constexpr auto expected_input = std::make_pair(0, 0);
 
 
-export auto part1(auto&& input) {
+export auto part1(auto const& input) {
 	return 0;
 }
 
-export auto part2(auto&& input) {
+export auto part2(auto const& input) {
 	return 0;
 }
