255bb6fce7e7029a953af78eabd02d0b1c50f648
Kenneth Gorking <kgorking@outlook.com>
2025-12-04 07:05:58 +0100

    day 3

--- 2025/day03/solution.cpp
+++ 2025/day03/solution.cpp
@@ -6,7 +6,7 @@ import std;
 static constexpr auto expected_sample = std::make_pair(  357, 3121910778619);
 static constexpr auto expected_input  = std::make_pair(16854, 167526011932478);
 
-// Solve it using a monotonic stack approach
+// Solve it using a monotonic stack
 static std::int64_t max_joltage_n(std::string_view battery_bank, int const s) {
 	int n = battery_bank.size() - s;
 
@@ -37,6 +37,39 @@ static std::int64_t max_joltage_n(std::string_view battery_bank, int const s) {
 	return result;
 }
 
+// Solve it using a bitset monotonic stack. Twice as slow as the vector version. Yay!
+static std::int64_t max_joltage_n_bit(std::string_view battery_bank, int const s) {
+	int n = battery_bank.size() - s;
+
+	std::bitset<100> mono;
+	int last_set = -1;
+
+	for (auto [index, battery] : battery_bank | std::views::enumerate) {
+		while (last_set != -1 && n > 0 && battery_bank[last_set] < battery) {
+			mono[last_set] = 0;
+			last_set = 100 - 1 - kg::clz(mono);
+			--n;
+		}
+
+		mono[index] = 1;
+		last_set = index;
+	}
+
+	while (n) {
+		last_set = 100 - 1 - kg::clz(mono);
+		mono[last_set] = 0;
+		--n;
+	}
+
+	std::int64_t result = 0;
+	kg::bit_iterate_set(mono, [&](int i) {
+		result *= 10;
+		result += battery_bank[i] - '0';
+	});
+
+	return result;
+}
+
 static auto part1(auto const& input) {
 	std::int64_t sum = 0;
 	for (std::string_view battery_bank : input) {
@@ -48,7 +81,7 @@ static auto part1(auto const& input) {
 static auto part2(auto const& input) {
 	std::int64_t sum = 0;
 	for (std::string_view battery_bank : input) {
-		sum += max_joltage_n(battery_bank, 12);
+		sum += max_joltage_n_bit(battery_bank, 12);
 	}
 	return sum;
 }
