ecac4534463b15e6908f4c4c3652e7a742828c57
Kenneth Gorking <kgorking@outlook.com>
2025-12-04 22:32:03 +0100

    Final_v2_for_real_this_time

--- 2025/day04/solution.cpp
+++ 2025/day04/solution.cpp
@@ -17,11 +17,10 @@ matrix_t count_adjacent_rolls(auto const& current_rolls) {
 	};
 
 	matrix_t mat {};
-
 	kg::grid grid_mat { mat };
 
-	for (int y = 0; y < current_rolls.size(); y++) {
-		for (int x = 0; x < current_rolls[0].size(); x++) {
+	for (int y = 1; y < current_rolls.size()-1; y++) {
+		for (int x = 1; x < current_rolls[0].size()-1; x++) {
 			if ('@' == current_rolls[y][x])
 				for (kg::pos2di pos{ x, y }; kg::pos2di const offset : neighbours_offsets)
 					grid_mat[pos + offset] += 1;
@@ -31,20 +30,12 @@ matrix_t count_adjacent_rolls(auto const& current_rolls) {
 }
 
 static auto part1(auto const& input) {
-	matrix_t current_rolls {};
-	for (int y = 1; y < input.size() - 1; y++) {
-		for (int x = 1; x < input[0].size() - 1; x++) {
-			if ('@' == input[y][x])
-				current_rolls[y][x] = '@';
-		}
-	}
-
-	matrix_t const roll_count = count_adjacent_rolls(current_rolls);
+	matrix_t const roll_count = count_adjacent_rolls(input);
 
 	int count = 0;
 	for (int y = 1; y<input.size() - 1; y++) {
 		for (int x = 1; x < input[0].size() - 1; x++) {
-			bool const is_roll = '@' == current_rolls[y][x];
+			bool const is_roll = '@' == input[y][x];
 			count += is_roll && (roll_count[y][x] < 4);
 		}
 	}
@@ -53,19 +44,13 @@ static auto part1(auto const& input) {
 }
 
 static auto part2(auto const& input) {
-	using neighbourhood = std::array<char*, 4>; // 24 bytes for 9 positions. 8 bytes for padding.
-	
-	// Pre-alloc and pre-fault memory for active rolls. Saves ~50us
-	int mem_needed = 0;
-	for (int y = 1; y < input.size() - 1; y++) {
-		for (int x = 1; x < input[0].size() - 1; x++)
-			mem_needed += '@' == input[y][x];
-	}
-	std::vector<neighbourhood> active_rolls(mem_needed);
-
-	// Stick pointers to paper rolls counts in a vector,
+	// Stick pointers to paper rolls counts in an array,
 	// along with the pointers to the neighbouring positions.
 	// This allows me to avoid grid lookups during processing.
+	using neighbourhood = std::array<char*, 3>; // 24 bytes to store a paper rolls entire neighbourhood.
+	std::array<neighbourhood, 13000> active_rolls{};
+
+	// Set up the initial roll counts and active rolls
 	matrix_t roll_count {};
 	std::size_t active = 0;
 
@@ -88,27 +73,27 @@ static auto part2(auto const& input) {
 		}
 	}
 
-	int total_removed = 0;
+	// Process active rolls until no more can be removed
 	std::size_t const initial_active = active;
+	int total_removed = 0;
 
 	while (true) {
 		std::size_t const last_active = active;
 
-		// Process all active rolls
 		for (std::size_t i = 0; i < active;) {
 			neighbourhood const& n = active_rolls[i];
 
-			auto const roll = n[1][1];
-			if (roll < 4) {
+			char const num_roll_neighbours = n[1][1];
+			if (num_roll_neighbours < 4) {
 				// Decrease the roll count for all neighbouring positions
 				n[0][0] -= 1; n[0][1] -= 1; n[0][2] -= 1;
 				n[1][0] -= 1;               n[1][2] -= 1;
 				n[2][0] -= 1; n[2][1] -= 1; n[2][2] -= 1;
-
-				// Remove the roll from the vector
+			
+				// Remove the roll from consideration
 				active_rolls[i] = active_rolls[--active];
 			} else {
-				// The roll remains in the vector
+				// Next roll
 				i++;
 			}
 		}
